9b49fcedb1f6286e42b24141e01274c8d366a6cd
Agustin <agustin.battaglino@mercadolibre.com>
2017-06-18 19:05:33 -0300

    UI added

--- src/uade/progra3/tpo/app/NavigatorMain.java
+++ src/uade/progra3/tpo/app/NavigatorMain.java
@@ -3,21 +3,26 @@ package uade.progra3.tpo.app;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import uade.progra3.tpo.model.Coordinate;
 import uade.progra3.tpo.model.Node;
 import uade.progra3.tpo.tda.MatrixPathFinderTDA;
 import uade.progra3.tpo.tda.MatrixPathFinderTDAImpl;
+import uade.progra3.tpo.ui.NavigatorWindow;
 import uade.progra3.tpo.util.MatrixUtils;
 
 public class NavigatorMain {
 
 	public static void main(String[] args) {
 		// Preparo la matriz
-		int[][] matrix = new int[5][5];
+		int[][] matrix = new int[25][25];
 		MatrixUtils.fillMatrix(matrix, 4);
 
-		Coordinate startCoordinate = new Coordinate(0, 0);
-		Coordinate endCoordinate = new Coordinate(4, 4);
+		Coordinate startCoordinate = new Coordinate(20, 0);
+		Coordinate endCoordinate = new Coordinate(10, 24);
 		MatrixPathFinderTDA tda = new MatrixPathFinderTDAImpl();
 		Node start = new Node(startCoordinate, matrix[startCoordinate.getX()][startCoordinate.getY()], 0,
 				MatrixUtils.getDistanceBetweenCoordinates(startCoordinate, endCoordinate));
@@ -26,9 +31,35 @@ public class NavigatorMain {
 		try {
 			List<Node> result = tda.getPath(matrix, start, end);
 			System.out.println("Solucion: " + result);
+			displayResults(matrix, result);
 		} catch (Exception e) {
 			System.out.print(e.getMessage());
 		}
+	}
 
+	private static void displayResults(int[][] matrix, List<Node> result) {
+		/* Use an appropriate Look and Feel */
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+		} catch (UnsupportedLookAndFeelException ex) {
+			ex.printStackTrace();
+		} catch (IllegalAccessException ex) {
+			ex.printStackTrace();
+		} catch (InstantiationException ex) {
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		UIManager.put("swing.boldMetal", Boolean.FALSE);
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				NavigatorWindow frame = new NavigatorWindow("PathFinder UI", matrix, result);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.addComponentsToPane(frame.getContentPane());
+				frame.pack();
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+			}
+		});
 	}
 }
---
+++ src/uade/progra3/tpo/ui/NavigatorWindow.java
@@ -0,0 +1,50 @@
+package uade.progra3.tpo.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+
+import uade.progra3.tpo.model.Coordinate;
+import uade.progra3.tpo.model.Node;
+
+public class NavigatorWindow extends JFrame {
+	private static final long serialVersionUID = -7225797436015174108L;
+	private GridLayout experimentLayout;
+	private int[][] matrix;
+	private List<Node> results;
+
+	public NavigatorWindow(String name, int[][] matrix, List<Node> results) {
+		super(name);
+		this.experimentLayout = new GridLayout(matrix.length, matrix[0].length);
+		setResizable(false);
+		this.matrix = matrix;
+		this.results = results;
+	}
+
+	public void addComponentsToPane(final Container pane) {
+		final JPanel compsToExperiment = new JPanel();
+		compsToExperiment.setLayout(experimentLayout);
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				Node node = new Node(new Coordinate(i, j), matrix[i][j], 0, 0);
+				JButton b = new JButton(" " + Integer.toString(matrix[i][j]) + " ");
+				b.setOpaque(true);
+				if(results.contains(node)) {
+					b.setBackground(Color.GREEN);
+				}
+				compsToExperiment.add(b);
+			}
+		}
+
+		pane.add(compsToExperiment, BorderLayout.EAST);
+		pane.add(new JSeparator(), BorderLayout.CENTER);
+	}
+}
