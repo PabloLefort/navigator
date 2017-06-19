e6820292b06f004a3f4523ec4a5934eb67ab71df
Agustin <agustin.battaglino@mercadolibre.com>
2017-06-18 21:23:03 -0300

    interactive matrix

--- src/uade/progra3/tpo/app/NavigatorMain.java
+++ src/uade/progra3/tpo/app/NavigatorMain.java
@@ -23,43 +23,16 @@ public class NavigatorMain {
 
 		Coordinate startCoordinate = new Coordinate(0, 0);
 		Coordinate endCoordinate = new Coordinate(24, 24);
-		MatrixPathFinderTDA tda = new MatrixPathFinderTDAImpl();
 		Node start = new Node(startCoordinate, matrix[startCoordinate.getX()][startCoordinate.getY()], 0,
 				MatrixUtils.getDistanceBetweenCoordinates(startCoordinate, endCoordinate));
 		Node end = new Node(endCoordinate, matrix[endCoordinate.getX()][endCoordinate.getY()], 0, 0);
 		System.out.println("Matriz: " + Arrays.deepToString(matrix));
 		try {
-			List<Node> result = tda.getPath(matrix, start, end);
-			System.out.println("Solucion: " + result);
-			displayResults(matrix, result);
+			// List<Node> result = tda.getPath(matrix, start, end);
+			// System.out.println("Solucion: " + result);
+			new NavigatorWindow("PathFinder UI", matrix, start, end).displayUI();
 		} catch (Exception e) {
 			System.out.print(e.getMessage());
 		}
-	}
-
-	private static void displayResults(int[][] matrix, List<Node> result) {
-		/* Use an appropriate Look and Feel */
-		try {
-			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-		} catch (UnsupportedLookAndFeelException ex) {
-			ex.printStackTrace();
-		} catch (IllegalAccessException ex) {
-			ex.printStackTrace();
-		} catch (InstantiationException ex) {
-			ex.printStackTrace();
-		} catch (ClassNotFoundException ex) {
-			ex.printStackTrace();
-		}
-		UIManager.put("swing.boldMetal", Boolean.FALSE);
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				NavigatorWindow frame = new NavigatorWindow("PathFinder UI", matrix, result);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.addComponentsToPane(frame.getContentPane());
-				frame.pack();
-				frame.setLocationRelativeTo(null);
-				frame.setVisible(true);
-			}
-		});
 	}
 }
--- src/uade/progra3/tpo/ui/NavigatorWindow.java
+++ src/uade/progra3/tpo/ui/NavigatorWindow.java
@@ -4,43 +4,69 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import uade.progra3.tpo.model.Coordinate;
 import uade.progra3.tpo.model.Node;
 import uade.progra3.tpo.tda.MatrixPathFinderTDA;
+import uade.progra3.tpo.tda.MatrixPathFinderTDAImpl;
 
 public class NavigatorWindow extends JFrame {
 	private static final long serialVersionUID = -7225797436015174108L;
 	private GridLayout experimentLayout;
 	private int[][] matrix;
 	private List<Node> results;
+	private MatrixPathFinderTDA tda;
+	private Node start, end;
 
-	public NavigatorWindow(String name, int[][] matrix, List<Node> results) {
+	public void displayUI() throws Exception {
+		Container container = this.getContentPane();
+		container.removeAll();
+		this.tda = new MatrixPathFinderTDAImpl();
+		this.results = tda.getPath(matrix, start, end);
+		addComponentsToPane(container);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	public NavigatorWindow(String name, int[][] matrix, Node start, Node end) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		super(name);
+		this.start = start;
+		this.end = end;
+		UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+		UIManager.put("swing.boldMetal", Boolean.FALSE);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.experimentLayout = new GridLayout(matrix.length, matrix[0].length);
 		setResizable(false);
+
 		this.matrix = matrix;
 		this.results = results;
 	}
 
-	public void addComponentsToPane(final Container pane) {
+	private void addComponentsToPane(final Container pane) {
 		final JPanel compsToExperiment = new JPanel();
 		compsToExperiment.setLayout(experimentLayout);
 
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
-				Node node = new Node(new Coordinate(i, j), matrix[i][j], 0, 0);
-				JButton b = new JButton(Integer.toString(matrix[i][j]));
+				int value = matrix[i][j];
+				Node node = new Node(new Coordinate(i, j), value, 0, 0);
+				JButton b = new JButton(Integer.toString(value));
+				b.addActionListener(new ButtonActionListener(new Coordinate(i, j)));
 				b.setOpaque(true);
-				if(results.contains(node)) {
+				if (results.contains(node)) {
 					b.setBackground(Color.GREEN);
-				} else if(node.getValue() == MatrixPathFinderTDA.FORBIDDEN_VALUE) {
+				} else if (node.getValue() == MatrixPathFinderTDA.FORBIDDEN_VALUE) {
 					b.setBackground(Color.RED);
 				}
 				compsToExperiment.add(b);
@@ -48,6 +74,32 @@ public class NavigatorWindow extends JFrame {
 		}
 
 		pane.add(compsToExperiment, BorderLayout.EAST);
-		pane.add(new JSeparator(), BorderLayout.CENTER);
+	}
+
+	private class ButtonActionListener implements ActionListener {
+
+		private Coordinate coord;
+
+		public ButtonActionListener(Coordinate coord) {
+			this.coord = coord;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton button = (JButton) e.getSource();
+			int value = Integer.parseInt(button.getText());
+			if (value == MatrixPathFinderTDA.FORBIDDEN_VALUE) {
+				value = 0;
+			} else {
+				value = value + 1;
+			}
+			matrix[this.coord.getX()][this.coord.getY()] = value;
+			try {
+				displayUI();
+			} catch (Exception e1) {
+				System.out.println(e1.getMessage());
+				System.exit(-1);
+			}
+		}
 	}
 }
